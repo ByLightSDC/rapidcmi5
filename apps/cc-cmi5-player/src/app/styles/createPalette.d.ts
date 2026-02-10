07d8bb452c8b2ec2554eb1f3f9b0d218cc79644f
Michelle Gabele <michelle.gabele@bylight.com>
2026-02-10 11:43:54 -0500

    CCUI-2249 #comment Minor Theme Tweak

--- apps/cc-cmi5-player/src/app/styles/createPalette.d.ts
+++ apps/cc-cmi5-player/src/app/styles/createPalette.d.ts
@@ -70,6 +70,8 @@ declare module '@mui/material/styles' {
       disabledColor: React.CSSProperties['color'];
       gradient: React.CSSProperties['color'];
       minorBackgroundColor: React.CSSProperties['color'];
+      indicatorColor: React.CSSProperties['color'];
+      iconColor: React.CSSProperties['color'];
     };
     card: {
       default: React.CSSProperties['color'];
