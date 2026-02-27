7a17b127f6195d4b02765b9d6f842d7ff1e2076d
Megan Bohland <megan.bohland@bylight.com>
2026-02-27 12:46:42 -0500

    Created file for hook

--- packages/rapid-cmi5/src/lib/design-tools/rapidcmi5_mdx/plugins/image/EditImageToolbar.tsx
+++ packages/rapid-cmi5/src/lib/design-tools/rapidcmi5_mdx/plugins/image/EditImageToolbar.tsx
@@ -29,52 +29,7 @@ import {
 
 import { StyleDialog } from './StyleDialog';
 import { Z_INDEX } from './index';
-
-/**
- *  This will allow Style Dialog to be called from the palette button. Effectively separated from ImageDialog.
- * In the future we could move this to a 'hooks' file.
- * @param nodeKey - The image node to be edited.
- * @returns An object containing the current style string and a setter function.
- */
-function useImageStyle(nodeKey: string) {
-  const [editor] = useLexicalComposerContext();
-  const [imageStyle, setImageStyle] = useState<string>('');
-
-  useEffect(() => {
-    if (!imageStyle) return;
-
-    // Otherwise, update.
-    editor.update(() => {
-      const node = $getNodeByKey(nodeKey);
-      if (!node) return;
-
-      // Attributes such as style and width exist in'rest'.
-      if (
-        'getRest' in node &&
-        'setRest' in node &&
-        typeof node.getRest === 'function' &&
-        typeof node.setRest === 'function'
-      ) {
-        //Get the current set attributes.
-        const rest = node.getRest() ?? [];
-
-        // Update them with changes.
-        const nextRest = [
-          ...rest.filter((attr: any) => attr.name !== 'style'),
-          {
-            type: 'mdxJsxAttribute',
-            name: 'style',
-            value: imageStyle,
-          },
-        ];
-        // Set and render update.
-        node.setRest(nextRest);
-      }
-    });
-  }, [imageStyle, editor, nodeKey]);
-
-  return { imageStyle, setImageStyle };
-}
+import { useImageStyle } from './ImageHooks';
 
 export interface EditImageToolbarProps {
   nodeKey: string;
